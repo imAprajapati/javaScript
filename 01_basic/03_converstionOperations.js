f84c25123cc0d96a80548932a0b603ebe2a2a13a
Jokhendra Prajapati <49206910+imAprajapati@users.noreply.github.com>
2023-09-13 16:10:40 +0000

    converstion

---
+++ 01_basic/03_converstionOperations.js
@@ -0,0 +1,23 @@
+let score = "33abc"
+
+console.log(typeof score) // string
+let valueInNumber = Number(screen) // NaN
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1 ; false => 0
+
+let isLogedIn = "jokhendra";
+let booleanIsLoggedIn = Boolean(isLogedIn);
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false;
+// "jokhedra" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
